30f88daa7c7e544dc27283d8de289cf8ec4e0bef
aframirez177 <af.ramirez1772@gmail.com>
2024-08-31 22:53:53 -0500

    usando sass

--- js/ham_menu.js
+++ js/ham_menu.js
@@ -6,3 +6,12 @@ hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
 });
+
+const expandCalc = document.querySelector(".colapsable-cargo-button");
+
+const offScreenItems = document.querySelector(".checkbox-group");
+
+expandCalc.addEventListener("click", () => {
+    expandCalc.classList.toggle("active");
+    offScreenItems.classList.toggle("inactive");
+});
